f111f16ab992ba1b7121eb8a69ef76e367e8ed24
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-09-25 14:50:01 +0000

    test with http

--- src/test/java/eu/trentorise/smartcampus/services/ou/test/TestXSS.java
+++ src/test/java/eu/trentorise/smartcampus/services/ou/test/TestXSS.java
@@ -10,13 +10,12 @@ public class TestXSS {
 	public static void main(String[] args) throws Exception {
 
 		XSSTestHelper helper = new XSSTestHelper();
-		XSSData data = helper.processXHTMLFromReader(new FileReader(
-				"src/test/resources/locations.html"), new FileReader(
+		XSSData 
+		data = helper.processXHTMLFromHTTP("http://www.operauni.tn.it/cms-01.00/articolo.asp?IDcms=8581&s=39&l=IT", new FileReader(
 				"src/main/resources/service/ou/locations.xml"));
 		System.err.println(data);
 		
-//		data = helper.processXHTMLFromReader(new FileReader(
-//				"src/test/resources/detail.html"), new FileReader(
+//		data = helper.processXHTMLFromHTTP("http://www.operauni.tn.it/cms-01.00/articolo.asp?idcms=353&s=39",  new FileReader(
 //				"src/main/resources/service/ou/detail.xml"));
 //		System.err.println(data);
 
