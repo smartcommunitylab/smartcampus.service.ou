e24be91524368cd89c9ffd9017f0f55777fa7683
adami <adami@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-09-26 13:54:48 +0000

    Connector need CP1252, XSS output UTF-8

--- src/test/java/eu/trentorise/smartcampus/services/ou/test/TestXSS.java
+++ src/test/java/eu/trentorise/smartcampus/services/ou/test/TestXSS.java
@@ -4,20 +4,26 @@ import it.sayservice.platform.servicebus.test.XSSTestHelper;
 import it.sayservice.xss.api.data.XSSData;
 
 import java.io.FileReader;
+import java.nio.charset.Charset;
+
+
 
 public class TestXSS {
 
 	public static void main(String[] args) throws Exception {
 
 		XSSTestHelper helper = new XSSTestHelper();
-		XSSData 
-		data = helper.processXHTMLFromHTTP("http://www.operauni.tn.it/cms-01.00/articolo.asp?IDcms=8581&s=39&l=IT", new FileReader(
-				"src/main/resources/service/ou/locations.xml"));
-		System.err.println(data);
 		
-//		data = helper.processXHTMLFromHTTP("http://www.operauni.tn.it/cms-01.00/articolo.asp?idcms=353&s=39",  new FileReader(
-//				"src/main/resources/service/ou/detail.xml"));
+		XSSData data = helper.processXHTMLFromHTTP("http://www.operauni.tn.it/cms-01.00/articolo.asp?IDcms=8581&s=39&l=IT", new FileReader("src/main/resources/service/ou/locations.xml"), "CP1252");
+		String d1 = new String(data.toString().getBytes(),Charset.forName("UTF-8"));
+		System.err.println(d1);
+		
+		
+//		XSSData data = helper.processXHTMLFromHTTP("http://www.operauni.tn.it/cms-01.00/articolo.asp?idcms=361&s=39",  new FileReader(
+//				"src/main/resources/service/ou/detail.xml"),"UTF-8");
 //		System.err.println(data);
 
 	}
 }
+
+//http://www.operauni.tn.it/cms-01.00/articolo.asp?idcms=123&s=39
